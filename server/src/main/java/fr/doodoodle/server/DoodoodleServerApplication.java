4ca8a7382a333b88717dce54fcc161513a437ba8
Julian <darksnoopy42@gmail.com>
2016-04-09 11:32:40 +0200

    feat(server): spring boot initialization

---
+++ server/src/main/java/fr/doodoodle/server/DoodoodleServerApplication.java
@@ -0,0 +1,12 @@
+package fr.doodoodle.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoodoodleServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoodoodleServerApplication.class, args);
+	}
+}
---
+++ server/src/test/java/fr/doodoodle/server/DoodoodleServerApplicationTests.java
@@ -0,0 +1,18 @@
+package fr.doodoodle.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DoodoodleServerApplication.class)
+@WebAppConfiguration
+public class DoodoodleServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
