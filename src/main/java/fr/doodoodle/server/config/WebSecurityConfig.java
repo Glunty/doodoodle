41cdd432a5255f1225f09e47b835d519522c3541
Glunty <s.thiebaud@gmail.com>
2016-04-10 00:24:37 +0200

    TSR : bugfix

--- src/main/java/fr/doodoodle/server/config/WebSecurityConfig.java
+++ src/main/java/fr/doodoodle/server/config/WebSecurityConfig.java
@@ -38,7 +38,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/").allowedOrigins("*");
+                registry.addMapping("/**").allowedOrigins("*");
             }
         };
     }
